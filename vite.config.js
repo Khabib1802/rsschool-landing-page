99cf89f86cc456c1d5d69b44bad52b185a8f5140
Khabib Gadzhimagomedov <khabib.x.gadzhimagomedov@gmail.com>
2026-09-21 22:59:55 +0300

    build: set base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
+  base: "/rsschool-landing-page/",
   build: {
     sourcemap: true,
     rollupOptions: {
